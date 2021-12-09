b362eaa9317e81c041d505ae0d0b16efed0f2237
sanapala12 <59277514+sanapala12@users.noreply.github.com>
2021-12-09 14:11:02 +0530

    Add files via upload

---
+++ shippingapi/src/main/java/com/eric/shippingapi/ShippingapiApplication.java
@@ -0,0 +1,30 @@
+package com.eric.shippingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+
+import com.eric.shippingapi.facades.ShippingFacade;
+
+import com.eric.shippingapi.models.Payment;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@Slf4j
+@EnableBinding(ShippingFacade.class)
+public class ShippingapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingapiApplication.class, args);
+	}
+
+	@StreamListener(target = ShippingFacade.INPUT)
+    public void handlePayment(@Payload Payment payment) {
+     log.info("invoked.....");   
+	 log.info("Received Payment details: {}",payment);
+    }
+
+}
---
+++ shippingapi/src/main/java/com/eric/shippingapi/models/PaymentMode.java
@@ -0,0 +1,5 @@
+package com.eric.shippingapi.models;
+
+public enum PaymentMode {
+ Cash,Cheque,Wallet,DebitCard,CreditCard,NetBanking
+}
